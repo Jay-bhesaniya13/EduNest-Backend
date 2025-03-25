6dd205ddcf5bf81c4e6791540b1083e9879b09df
Jay-bhesaniya13 <jaybhesaniya60@gmail.com>
2025-03-25 22:55:38 +0530

    leaderboard get

--- routes/leaderBoardRoutes.js
+++ routes/leaderBoardRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const leaderboardController = require("../controllers/leaderBoardController");
  // Get leaderboard for a specific quiz
- router.get("/quiz/:quizId?", leaderboardController.getLeaderboard);
+ router.get("/:quizId?", leaderboardController.getLeaderboard);
  // Update leaderboard when a student completes a quiz
 router.post("/update", leaderboardController.updateLeaderboard);
  module.exports = router;
