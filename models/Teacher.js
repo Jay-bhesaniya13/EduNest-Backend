e93ce476fc881e0e6fe35fd3fd630e2acb69f3ff
Jay-bhesaniya13 <jaybhesaniya60@gmail.com>
2025-04-26 23:25:22 +0530

    bug4

--- models/Teacher.js
+++ models/Teacher.js
@@ -6,7 +6,7 @@ const teacherSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  contactNumber: { type: String },
+  contactNumber: { type: Number },
   profilepicURL: { type: String, required: true },
   about: { type: String },
   areas_of_expertise: [{ type: String }],
--- models/TempTeacher.js
+++ models/TempTeacher.js
@@ -4,7 +4,7 @@ const tempTeacherSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  contactNumber: { type: String },
+  contactNumber: { type: Number },
   profilepicURL: { type: String, required: true },
   about: { type: String },
   areas_of_expertise: [{ type: String }],
